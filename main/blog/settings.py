9337ca4936240e32e4ebcfcff838594fbce623a9
Hari <harikrishnan51688@gmail.com>
2022-09-16 15:37:58 +0530

    fix

--- main/blog/settings.py
+++ main/blog/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
 
     'post',
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
